b5f8f6d5c690d2beaa7c2d2daa00d22de2a572ba
MOHAMEDABDULKARIM27 <MohamedAbdulkarim27@gmail.com>
2021-03-02 23:25:28 -0500

    modified App.js to say {Mohamed AbdulKarim}

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        My name is Mohamed Abdulkarim and I'm going to master React!
+        My name is Mohamed AbdulKarim and I'm going to master React!
         </p>
         <a
           className="App-link"
